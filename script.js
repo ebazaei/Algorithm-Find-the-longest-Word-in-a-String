e39a3bef983ce200d3a513032808e386d35615ef
ehsan bazaei <bazaeiehsan@gmail.com>
2022-01-06 17:30:54 +0100

    start

---
+++ script.js
@@ -0,0 +1,8 @@
+// Tuesday: Find the longest Word in a String
+// Return the length of the longest word in the provided sentence.
+
+// Your response should be a number. For these algorithms you are allowed to use the split() method.
+
+// findLongestWord("The quick brown fox jumped over the lazy dog")should return a number.
+// findLongestWord("The quick brown fox jumped over the lazy dog")should return 6.
+// findLongestWord("May the force be with you") should return 5.
